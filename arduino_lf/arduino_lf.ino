d7e3c68946db82ae6ae7be1f178ac438cac02595
Dominic <dando.ds11@gmail.com>
2021-01-08 20:49:37 +0200

    Working version with PID controller

--- arduino_lf/arduino_lf.ino
+++ arduino_lf/arduino_lf.ino
@@ -1,94 +1,337 @@
-void citesteSenzori();
-void controlMotoare();
-void ajustareTraiectorie();
+double error;
+double lastError;
+double errorDiff;
+double errorInt;
+
+#define KP 17.00
+#define KD 600.00
+#define KI 0.00
+#define baseSpeed 80.00
+#define nearLineModifier 0.50
+
+/*
+best 1
+KP 6
+KD 500 
+KI 0.1
+base speed 25
+
+best 2 
+#define KP 6.0
+#define KD 560
+#define KI 0.1
+#define baseSpeed 25.0
+#define nearLineModifier 1.0
 
 
-double SA0;
-double SA1;
-double SA2;
-double SA3;
-double SA4;
-double ajustare;
-int output_PD5 = 5;
-int output_PD4 = 4;
-int input_SA1 = A1;
-int output_PD7 = 7;
-int input_SA0 = A0;
-int output_PD6 = 6;
-int input_SA3 = A3;
-int input_SA2 = A2;
-int input_SA4 = A4;
-int led_L = LED_BUILTIN;
-int output_PWM10 = 10;
-int output_PWM9 = 9;
+best 3 
+#define KP 3.00
+#define KD 580.0
+#define KI 0.0
+#define baseSpeed 25.0
+#define nearLineModifier 1.0
 
-void citesteSenzori() {
-    SA0 = analogRead(input_SA0);
-    SA1 = analogRead(input_SA1);
-    SA2 = analogRead(input_SA2);
-    SA3 = analogRead(input_SA3);
-    SA4 = analogRead(input_SA4);
-}
+19s
+best 4
+#define KP 6.00
+#define KD 580.00
+#define KI 0.00
+#define baseSpeed 30.00
+#define nearLineModifier 0.5
 
-void controlMotoare() {
-    digitalWrite(output_PD4, (int)1);
-    digitalWrite(output_PD5, (int)0);
-    digitalWrite(output_PD6, (int)0);
-    digitalWrite(output_PD7, (int)1);
-    if (ajustare == 0 ) {
-        analogWrite(output_PWM9, (int)60);
-        analogWrite(output_PWM10, (int)60);
-    } else {
-        if (ajustare == -1 ) {
-            analogWrite(output_PWM9, (int)60);
-            analogWrite(output_PWM10, (int)0);
-        } else {
-            analogWrite(output_PWM9, (int)0);
-            analogWrite(output_PWM10, (int)60);
-        }
+16.50s
+
+best 5
+#define KP 6.00
+#define KD 580.00
+#define KI 0.00
+#define baseSpeed 30.00
+#define nearLineModifier 0.5
+
+15.05s
+best 6 - extensie 
+#define KP 6.00
+#define KD 580.00
+#define KI 0.00
+#define baseSpeed 30.00
+#define nearLineModifier 0.5
+
+#define KP 17.00
+#define KD 600.00
+#define KI 0.00
+#define baseSpeed 70.00
+#define nearLineModifier 0.50
+10.6s
+
+///prototype
+#define KP 31.00
+#define KD 810.00
+#define KI 0.00
+#define baseSpeed 100.00
+#define nearLineModifier 0.60
+
+best of best 
+#define KP 17.00
+#define KD 600.00
+#define KI 0.00
+#define baseSpeed 80.00
+#define nearLineModifier 0.50
+
+10.3s
+*/
+
+struct motor
+{
+
+  int motorControllerForwardPIN;
+  int motorControllerBackwardPIN;
+  int motorControllerSpeedPIN;
+  int internalPower;
+  int mappedPower;
+  bool internalOrientation = 0;
+
+  void setOrientation(bool orientation)
+  {
+    internalOrientation = orientation;
+  }
+
+  void setPins(int forwardPIN, int backwardPIN, int speedPWMPIN)
+  {
+    motorControllerForwardPIN = forwardPIN;
+    motorControllerBackwardPIN = backwardPIN;
+    motorControllerSpeedPIN = speedPWMPIN;
+    pinMode(motorControllerForwardPIN, OUTPUT);
+    pinMode(motorControllerBackwardPIN, OUTPUT);
+    pinMode(motorControllerSpeedPIN, OUTPUT);
+  }
+
+  void setPower(int power)
+  {
+    power = constrain(power, -100, 100);
+    mappedPower = power;
+    if (internalOrientation)
+      power *= -1;
+    internalPower = map(abs(power), 0, 100, 0, 255);
+    if (power < 0)
+    {
+      digitalWrite(motorControllerForwardPIN, LOW);
+      digitalWrite(motorControllerBackwardPIN, HIGH);
     }
-}
-
-void ajustareTraiectorie() {
-    if (SA2 < 200 ) {
-        ajustare = 0;
-    } else {
-        if ( (SA0 < 200 ) || (SA1 < 200 ) ) {
-            ajustare = 1;
-        } else {
-            if ( (SA3 < 200 ) || (SA4 < 200 ) ) {
-                ajustare = -1;
-            }
-        }
+    else
+    {
+      digitalWrite(motorControllerBackwardPIN, LOW);
+      digitalWrite(motorControllerForwardPIN, HIGH);
     }
-}
+    analogWrite(motorControllerSpeedPIN, internalPower);
+  }
+};
+
+motor Right;
+motor Left;
+
+struct lineSensor
+{
+  bool leftValue;
+  bool leftCenterValue;
+  bool centerValue;
+  bool rightCenterValue;
+  bool rightValue;
+
+  double globalLineValue = 0;
+  String lineOutput;
+  int lastGlobalLineValue = 0;
+  int leftSensorPIN;
+  int leftCenterSensorPIN;
+  int centerSensorPIN;
+  int rightCenterSensorPIN;
+  int rightSensorPIN;
+
+  void setPins(int left, int leftCenter, int center, int rightCenter, int right)
+  {
+    leftSensorPIN = left;
+    leftCenterSensorPIN = leftCenter;
+    centerSensorPIN = center;
+    rightCenterSensorPIN = rightCenter;
+    rightSensorPIN = right;
+    pinMode(leftSensorPIN, INPUT);
+    pinMode(leftCenterSensorPIN, INPUT);
+    pinMode(centerSensorPIN, INPUT);
+    pinMode(rightCenterSensorPIN, INPUT);
+    pinMode(rightSensorPIN, INPUT);
+  }
+
+   void LineValueAnalyzer()
+  {
+     if (!noDetection())
+      lastGlobalLineValue = globalLineValue;
+    leftValue = !digitalRead(leftSensorPIN);
+    leftCenterValue = !digitalRead(leftCenterSensorPIN);
+    centerValue = !digitalRead(centerSensorPIN);
+    rightCenterValue = !digitalRead(rightCenterSensorPIN);
+    rightValue = !digitalRead(rightSensorPIN);
+    lineOutput = (String)leftValue + (String)leftCenterValue + (String)centerValue + (String)rightCenterValue + (String)rightValue;
+    //Serial.println(lineOutput);
+    if (lineOutput == "10000")
+    {
+      globalLineValue = -6;//-5
+    }
+    else if (lineOutput == "11000")
+    {
+      globalLineValue = -5;//-3
+    }
+    else if (lineOutput == "01000")
+    {
+      globalLineValue = -2*nearLineModifier;
+    }
+    else if (lineOutput == "01100")
+    {
+      globalLineValue = -1*nearLineModifier;
+    }
+    else if (lineOutput == "00100")
+    {
+      globalLineValue = 0;
+    }
+    else if (lineOutput == "00110")
+    {
+      globalLineValue = 1*nearLineModifier;
+    }
+    else if (lineOutput == "00010")
+    {
+      globalLineValue = 2*nearLineModifier;
+    }
+    else if (lineOutput == "00011")
+    {
+      globalLineValue = 5;//3
+    }
+    else if (lineOutput == "00001")
+    {
+      globalLineValue = 6;//5
+    }
+    else if (lineOutput == "11100" || lineOutput == "11110"||lineOutput=="10100"||lineOutput=="10010")
+    {
+      globalLineValue = -6;
+    }
+    else if (lineOutput == "00111" || lineOutput == "01111"||lineOutput=="00101"||lineOutput=="01001")
+    {
+      globalLineValue = 6;
+    }
+    else if (lineOutput == "11111" || lineOutput == "01110"  || lineOutput=="01010")
+    {
+      globalLineValue = lastGlobalLineValue;
+    }
+
+   
+  } 
+
+  /* void LineValueAnalyzer(){
+    leftValue = digitalRead(leftSensorPIN);
+    leftCenterValue = digitalRead(leftCenterSensorPIN);
+    centerValue = digitalRead(centerSensorPIN);
+    rightCenterValue = digitalRead(rightCenterSensorPIN);
+    rightValue = digitalRead(rightSensorPIN);
+    globalLineValue = (double)((3)*leftValue+(1)*leftCenterValue+(-1)*rightCenterValue+(-3)*rightValue);
+    if(!noDetection())
+    lastGlobalLineValue = globalLineValue;
+  } 
+ */
+  bool noDetection()
+  {
+    leftValue = digitalRead(leftSensorPIN);
+    leftCenterValue = digitalRead(leftCenterSensorPIN);
+    centerValue = digitalRead(centerSensorPIN);
+    rightCenterValue = digitalRead(rightCenterSensorPIN);
+    rightValue = digitalRead(rightSensorPIN);
+    if (leftValue == 1 && leftCenterValue == 1 && centerValue == 1 && rightCenterValue == 1 && rightValue == 1)
+      return 1;
+    else
+      return 0;
+  }
+
+  int returnLineValue()
+  {
+    if (noDetection())
+    {
+      Serial.println(lastGlobalLineValue);
+      return lastGlobalLineValue;
+    }
+    else
+    {
+      Serial.println(globalLineValue);
+      return globalLineValue;
+    }
+  }
+
+  void SerialLineAnalyzer(String command)
+  {
+    LineValueAnalyzer();
+
+    if (command == "every sensor")
+    {
+      Serial.print(leftValue);
+      Serial.print(" ");
+      Serial.print(leftCenterValue);
+      Serial.print(" ");
+      Serial.print(centerValue);
+      Serial.print(" ");
+      Serial.print(rightCenterValue);
+      Serial.print(" ");
+      Serial.print(rightValue);
+      Serial.print("\n");
+    }
+    if (command == "all")
+    {
+      Serial.print(leftValue);
+      Serial.print(" ");
+      Serial.print(leftCenterValue);
+      Serial.print(" ");
+      Serial.print(centerValue);
+      Serial.print(" ");
+      Serial.print(rightCenterValue);
+      Serial.print(" ");
+      Serial.print(rightValue);
+      Serial.print("       ");
+      Serial.print(globalLineValue);
+      Serial.print(" ");
+    }
+    if (command == "value")
+    {
+      Serial.print(globalLineValue);
+      Serial.print("\n");
+    }
+  }
+};
+
+lineSensor lineSensorModule;
 
 void setup()
 {
-    pinMode(output_PD5, OUTPUT);
-    pinMode(output_PD4, OUTPUT);
-    pinMode(input_SA1, INPUT);
-    pinMode(output_PD7, OUTPUT);
-    pinMode(input_SA0, INPUT);
-    pinMode(output_PD6, OUTPUT);
-    pinMode(input_SA3, INPUT);
-    pinMode(input_SA2, INPUT);
-    pinMode(input_SA4, INPUT);
-    pinMode(led_L, OUTPUT);
-    pinMode(output_PWM10, OUTPUT);
-    pinMode(output_PWM9, OUTPUT);
-    SA0 = 0;
-    SA1 = 0;
-    SA2 = 0;
-    SA3 = 0;
-    SA4 = 0;
-    ajustare = 0;
+  Serial.begin(9600);
+  Right.setOrientation(0);
+  Left.setOrientation(1);
+  Right.setPins(11, 7, 9);
+  Left.setPins(12, 13, 10);
+  lineSensorModule.setPins(2, 3, 4, 5, 6);
+}
+
+double output;
+
+double PID(int input)
+{
+
+  error = error * 0.4 + input * 0.6;
+  errorDiff = error - lastError;
+  errorInt = constrain(error + errorInt, -20, 20);
+  output = KP * error + KD * errorDiff + KI * errorInt;
+  lastError = error;
+
+  return output;
 }
 
 void loop()
 {
-    ajustareTraiectorie();
-    citesteSenzori();
-    controlMotoare();
-    delay(10);
+  lineSensorModule.LineValueAnalyzer();
+  //Serial.println("LEFT   Internal Power : "+(String)Left.internalPower+"  Power:"+(String)Left.mappedPower +
+  //               "     RIGHT   Internal Power : "+(String)Right.internalPower+"  Power:"+(String)Right.mappedPower);
+  Left.setPower(baseSpeed - PID(lineSensorModule.returnLineValue()));
+  Right.setPower(baseSpeed + PID(lineSensorModule.returnLineValue()));
 }
